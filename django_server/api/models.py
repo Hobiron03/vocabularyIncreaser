5e6710717c669577d4e7c5e1b4acdc2a548edd89
HOBIRON <yudainepnep@icloud.com>
2020-05-11 00:14:00 +0900

    Implement function that add posted new word to database

--- django_server/api/models.py
+++ django_server/api/models.py
@@ -6,10 +6,10 @@ from django.db import models
 class Word(models.Model):
     user_id = models.IntegerField()
     word = models.CharField(max_length=100)
-    mean = models.CharField(max_length=100)
-    pronounce = models.CharField(max_length=100)
-    genre = models.CharField(max_length=100)
-    color = models.CharField(max_length=100)
+    mean = models.CharField(max_length=100, blank=True, null=True)
+    pronounce = models.CharField(max_length=100,  blank=True, null=True)
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    color = models.CharField(max_length=100,  blank=True, null=True)
 
     def __str__(self):
         return self.word
--- django_server/api/views.py
+++ django_server/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Word
 from .serializers import WordSerializer
 from rest_framework.response import Response
+from rest_framework import status
 from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 import json
@@ -30,5 +31,16 @@ class MyWordAPIView(generics.ListAPIView):
 
         return HttpResponse(qs_json)
 
-    # queryset = Word.objects.filter(user_id=id)
-    # serializer_class = WordSerializer
+    def post(self, request):
+        new_word = Word.objects.create(
+            user_id=request.user.id,
+            word=request.POST.get('word'),
+            mean=request.POST.get('mean'),
+            pronounce=request.POST.get('pronounce'),
+            genre=request.POST.get('genre'),
+            color=request.POST.get('color'),
+        )
+
+        new_word.save()
+
+        return Response(status=status.HTTP_201_CREATED)
