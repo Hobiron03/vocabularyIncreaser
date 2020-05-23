0f686f4ebf83c356d0a87f44f727ed21ecb1ce69
HOBIRON <yudainepnep@icloud.com>
2020-05-23 19:19:44 +0900

    push other file

--- reactFront/wordcollection/src/APIServerLocation.tsx
+++ reactFront/wordcollection/src/APIServerLocation.tsx
@@ -1,2 +1,3 @@
-const SERVER_LOCATION = "http://127.0.0.1:8000/";
+// const SERVER_LOCATION = "http://127.0.0.1:8000/";
+const SERVER_LOCATION = "https://word-collection-api.herokuapp.com/";
 export default SERVER_LOCATION;
