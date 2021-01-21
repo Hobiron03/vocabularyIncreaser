eea12682e4e31d0aa7d7966f5ae21ac088089e56
HOBIRON <yudainepnep@icloud.com>
2021-01-21 15:25:31 +0900

    style: コンポーネントのスタイルを修正

--- reactFront/wordcollection/src/components/Content/Content.tsx
+++ reactFront/wordcollection/src/components/Content/Content.tsx
@@ -1,21 +1,19 @@
-import React, { useEffect, useState, useContext } from 'react';
-import './Content.css';
-import Card from '../Card/Card';
-import AppContext from '../../contexts/AppContext';
+import React, { useEffect, useState, useContext } from "react";
+import "./Content.css";
+import Card from "../Card/Card";
+import AppContext from "../../contexts/AppContext";
 import {
   ADD_NEW_WORD,
   SET_CURRENT_GENRE,
   DELETE_ALL_WORD,
-} from '../../actions';
+} from "../../actions";
 
-import {
-  useHistory,
-} from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import axios from 'axios';
-import apiServer from '../../APIServerLocation';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Fade from '@material-ui/core/Fade';
+import axios from "axios";
+import apiServer from "../../APIServerLocation";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Fade from "@material-ui/core/Fade";
 
 interface wordData {
   id: number;
@@ -25,7 +23,7 @@ interface wordData {
   pronounce: string;
   genre: string;
   color: string;
-};
+}
 
 const Content = (props) => {
   const history = useHistory();
@@ -36,29 +34,31 @@ const Content = (props) => {
   const initState = () => {
     dispatch({
       type: SET_CURRENT_GENRE,
-      currentGenre: 'ALL',
+      currentGenre: "ALL",
     });
 
     //ブラウザバックの時に増殖するのを防ぐ
     dispatch({
       type: DELETE_ALL_WORD,
     });
-  }
+  };
 
   //リロード時にログイン状態を取得
   useEffect(() => {
-    const jwt: string | null = localStorage.getItem('jwt');
+    const jwt: string | null = localStorage.getItem("jwt");
     if (jwt) {
-      axios.get(apiServer + 'api/validation/', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `JWT ${jwt}`
-        },
-      }).then(_ => {
-      }).catch(error => {
-        console.log(error);
-        history.push("/");
-      })
+      axios
+        .get(apiServer + "api/validation/", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `JWT ${jwt}`,
+          },
+        })
+        .then((_) => {})
+        .catch((error) => {
+          console.log(error);
+          history.push("/");
+        });
     } else {
       history.push("/");
     }
@@ -69,14 +69,15 @@ const Content = (props) => {
   useEffect(() => {
     initState();
     const fetchMyWordData = async () => {
-      const jwt = localStorage.getItem('jwt');
-      await axios.get(apiServer + 'api/fetchmyword/', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `JWT ${jwt}`
-        },
-      })
-        .then(response => {
+      const jwt = localStorage.getItem("jwt");
+      await axios
+        .get(apiServer + "api/fetchmyword/", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `JWT ${jwt}`,
+          },
+        })
+        .then((response) => {
           response.data.forEach((wordData) => {
             const word = wordData.fields;
             word.id = wordData.pk;
@@ -89,8 +90,8 @@ const Content = (props) => {
         })
         .catch((error) => {
           console.log(error);
-        })
-    }
+        });
+    };
     fetchMyWordData();
     // eslint-disable-next-line
   }, []);
@@ -102,36 +103,28 @@ const Content = (props) => {
           <CircularProgress></CircularProgress>
           <p>Now Loading</p>
         </div>
-      )
-    }
-    else {
+      );
+    } else {
       return (
         <div>
           <p>{state.words.length} words</p>
-          <Fade
-            in={!isLoading}
-            {...(!isLoading ? { timeout: 600 } : {})}
-          >
+          <Fade in={!isLoading} {...(!isLoading ? { timeout: 600 } : {})}>
             <div className="word-list">
-              {
-                state.words.map((data: wordData, index: number): JSX.Element | undefined => {
-                  if (data.word.includes(state.searchWord)) {
-                    return <Card key={index} wordData={data}></Card>
-                  }
-                  return undefined;
-                })
-              }
+              {state.words.map((data: wordData, index: number):
+                | JSX.Element
+                | undefined => {
+                if (data.word.includes(state.searchWord)) {
+                  return <Card key={index} wordData={data}></Card>;
+                }
+                return undefined;
+              })}
             </div>
           </Fade>
         </div>
       );
     }
-  }
-  return (
-    <div className="content">
-      {renderWordList()}
-    </div>
-  )
+  };
+  return <div className="content">{renderWordList()}</div>;
 };
 
-export default Content
+export default Content;
